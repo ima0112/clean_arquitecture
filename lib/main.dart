ac4b664337ab4508ed41dbf6116ed3f913732afc
ima0112 <grumpy0112@gmail.com>
2021-07-06 13:04:42 -0400

    - Agregando un print para probar github CI.

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    print("Hello World!");
     return MaterialApp(
       title: 'Material App',
       home: Scaffold(
